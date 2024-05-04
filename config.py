cdcd334916cc17af3b610f400b91982b4b4977ff
Unique <mujeem000@gmail.com>
2024-05-04 10:24:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-api_id = 29707621
-api_hash = "a1f31eeb31a0b9e36f551da6b8b85752"
-bot_token = "6634971116:AAGhU6IQBREjXdliqJr9CGh1ZCLpoYbGP5o"
-auth_users = "6707073614, 1406300051, 6801378994, 870101201"
+api_id = 21233086
+api_hash = "2892db755e485aaf8e55b4b70e8953f6"
+bot_token = "7109218345:AAHE_RTiW5-JcFJ36-vEkJNexxJm0cPjI_I"
+auth_users = "7107871207"
 sudo_users = [int(num) for num in auth_users.split(",")]
-osowner_users = "6707073614"
+osowner_users = "7107871207"
 owner_users = [int(num) for num in osowner_users.split(",")]

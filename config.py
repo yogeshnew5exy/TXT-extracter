e027c2cf40ec186cf29e6658a643a00cc83d0dda
Unique <mujeem000@gmail.com>
2024-06-03 16:48:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-api_id = 21233086
-api_hash = "2892db755e485aaf8e55b4b70e8953f6"
-bot_token = "7109218345:AAHE_RTiW5-JcFJ36-vEkJNexxJm0cPjI_I"
-auth_users = "7107871207"
+api_id = "21179966"
+api_hash = "d97919fb0a3c725e8bb2a25bbb37d57c"
+bot_token = "6796353747:AAGhACxmk8It_apZdjIb8VfjFj_Bl8S8Qm8"
+auth_users = "7326397503"
 sudo_users = [int(num) for num in auth_users.split(",")]
-osowner_users = "7107871207"
+osowner_users = "7326397503"
 owner_users = [int(num) for num in osowner_users.split(",")]
